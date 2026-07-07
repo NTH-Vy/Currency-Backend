93756621624527f826290b4f14a3c76a3b27dd5c
huynh0210 <phanhuynhcse0210@gamil.com>
2026-07-07 22:49:40 +0700

    new11

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@ Route::get('/', function () {
 
 // Kiểm tra biến môi trường
 Route::get('/debug-env', function () {
+    $apiKey = config('services.exchange_rate.api_key');
     return response()->json([
         'frontend_url' => config('app.frontend_url'),
         'app_env' => config('app.env'),
         'app_url' => config('app.url'),
+        'api_key_set' => !empty($apiKey),
+        'api_key_preview' => $apiKey ? substr($apiKey, 0, 4) . '***' . substr($apiKey, -4) : 'NOT SET',
     ]);
 });
 
